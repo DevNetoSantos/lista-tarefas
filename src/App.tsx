6dabc045d4e9093d07947d2edf9eb439b538f15b
DevNetoSantos <rasneto147@gmail.com>
2022-06-09 19:37:46 -0300

    base do pojeto pronta

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as C from './App.styles';
+
+const App = () => {
+  return(
+    <div>
+      <C.Container>
+        <C.Area>
+          <C.Header>
+            Lista de Tarefas
+          </C.Header>
+        </C.Area>
+      </C.Container>
+    </div>
+  );
+}
+
+export default App;
