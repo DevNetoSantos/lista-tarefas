25a7a761f278194616d09b07160485504a50e89b
DevNetoSantos <rasneto147@gmail.com>
2022-06-09 20:23:57 -0300

    lista item feita

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/item';
+import { ListItem } from './components/ListItem';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: "Dar banho no gato", done: false},
+    {id: 2, name: "Tomar café", done: true},
+  ]);
+
+
   return(
     <div>
       <C.Container>
         <C.Area>
-          <C.Header>
-            Lista de Tarefas
-          </C.Header>
+          <C.Header>Lista de Tarefas</C.Header>
+            {list.map((item, index) =>(
+              <ListItem key={index} item={item}/>
+            ))}
         </C.Area>
       </C.Container>
     </div>
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,18 @@
+import * as C from './styles';
+import {Item} from '../../types/item'
+import { useState } from 'react';
+
+
+type Props = {
+ item: Item
+}
+
+export const ListItem = ({item}: Props) => {
+    const [isCheckd, setIsCheckd] = useState(item.done);
+    return(
+        <C.Container done={isCheckd}>
+            <input type="checkbox" checked={isCheckd} onChange={e=> setIsCheckd(e.target.checked)}/>
+            <label>{item.name}</label>
+        </C.Container>
+    );
+}
